23f90fd7425779e76c2ec0c358af70542493ab69
dajiangsiming <3150313717@qq.com>
2026-01-14 16:32:08 +0800

    第三次提交-借还书管理界面

--- librarymanager.cpp
+++ librarymanager.cpp
@@ -214,3 +214,97 @@ void LibraryManager::createReaderManagementTab()
 
     tabWidget->addTab(readerTab, "读者管理");
 }
+
+void LibraryManager::createBorrowReturnTab()
+{
+    QWidget *borrowTab = new QWidget;
+    QHBoxLayout *mainLayout = new QHBoxLayout(borrowTab);
+
+    // 左侧：借还书操作
+    QWidget *operationWidget = new QWidget;
+    QVBoxLayout *operationLayout = new QVBoxLayout(operationWidget);
+
+    // 借书区域
+    QGroupBox *borrowGroup = new QGroupBox("借书");
+    QFormLayout *borrowLayout = new QFormLayout;
+
+    borrowBookId = new QLineEdit;
+    borrowLayout->addRow("图书ID:", borrowBookId);
+
+    borrowReaderId = new QLineEdit;
+    borrowLayout->addRow("读者ID:", borrowReaderId);
+
+    borrowDays = new QSpinBox;
+    borrowDays->setRange(1, 180);
+    borrowDays->setValue(30);
+    borrowLayout->addRow("借阅天数:", borrowDays);
+
+    QPushButton *borrowButton = new QPushButton("借书");
+    connect(borrowButton, &QPushButton::clicked, this, &LibraryManager::borrowBook);
+    borrowLayout->addRow(borrowButton);
+
+    borrowGroup->setLayout(borrowLayout);
+    operationLayout->addWidget(borrowGroup);
+
+    // 还书区域
+    QGroupBox *returnGroup = new QGroupBox("还书");
+    QFormLayout *returnLayout = new QFormLayout;
+
+    returnRecordId = new QLineEdit;
+    returnLayout->addRow("借阅记录ID:", returnRecordId);
+
+    QPushButton *returnButton = new QPushButton("还书");
+    connect(returnButton, &QPushButton::clicked, this, &LibraryManager::returnBook);
+    returnLayout->addRow(returnButton);
+
+    QPushButton *renewButton = new QPushButton("续借");
+    connect(renewButton, &QPushButton::clicked, this, &LibraryManager::renewBook);
+    returnLayout->addRow(renewButton);
+
+    returnGroup->setLayout(returnLayout);
+    operationLayout->addWidget(returnGroup);
+
+    // 逾期书籍查看
+    QPushButton *overdueButton = new QPushButton("查看逾期书籍");
+    connect(overdueButton, &QPushButton::clicked, this, &LibraryManager::showOverdueList);
+    operationLayout->addWidget(overdueButton);
+
+    operationLayout->addStretch();
+    mainLayout->addWidget(operationWidget, 1);
+
+    // 右侧：借阅记录表格
+    QWidget *recordWidget = new QWidget;
+    QVBoxLayout *recordLayout = new QVBoxLayout(recordWidget);
+
+    QLabel *recordLabel = new QLabel("借阅记录");
+    recordLabel->setAlignment(Qt::AlignCenter);
+    QFont font = recordLabel->font();
+    font.setPointSize(12);
+    font.setBold(true);
+    recordLabel->setFont(font);
+    recordLayout->addWidget(recordLabel);
+
+    borrowTableView = new QTableView;
+    borrowModel = new QSqlTableModel(this, db);
+    borrowModel->setTable("borrow_records");
+    borrowModel->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    borrowModel->setFilter("status = '借出'");
+    borrowModel->select();
+
+    // 设置表头
+    borrowModel->setHeaderData(0, Qt::Horizontal, "记录ID");
+    borrowModel->setHeaderData(1, Qt::Horizontal, "图书ID");
+    borrowModel->setHeaderData(2, Qt::Horizontal, "读者ID");
+    borrowModel->setHeaderData(3, Qt::Horizontal, "借书日期");
+    borrowModel->setHeaderData(4, Qt::Horizontal, "应还日期");
+    borrowModel->setHeaderData(5, Qt::Horizontal, "状态");
+
+    borrowTableView->setModel(borrowModel);
+    borrowTableView->setSelectionBehavior(QAbstractItemView::SelectRows);
+    borrowTableView->resizeColumnsToContents();
+
+    recordLayout->addWidget(borrowTableView);
+    mainLayout->addWidget(recordWidget, 2);
+
+    tabWidget->addTab(borrowTab, "借还书管理");
+}
