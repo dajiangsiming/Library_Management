b02b39a8d9e27f88c4e446899fcbc3bfb8143a9e
dajiangsiming <3150313717@qq.com>
2026-01-14 15:45:06 +0800

    第一次提交-主界面和图书管理界面设计

---
+++ librarymanager.h
@@ -0,0 +1,132 @@
+﻿// librarymanager.h
+#ifndef LIBRARYMANAGER_H
+#define LIBRARYMANAGER_H
+
+#include <QMainWindow>
+#include <QSqlDatabase>
+#include <QSqlTableModel>
+#include <QStandardItemModel>
+#include <QTimer>
+#include <QSystemTrayIcon>
+
+class QTabWidget;
+class QTableView;
+class QLineEdit;
+class QComboBox;
+class QPushButton;
+class QDateEdit;
+class QLabel;
+class QTextEdit;
+class QGroupBox;
+class QSpinBox;
+class QCheckBox;
+
+class LibraryManager : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    LibraryManager(QWidget *parent = nullptr);
+    ~LibraryManager();
+
+private slots:
+    // 图书管理
+    void addBook();
+    void editBook();
+    void deleteBook();
+    void searchBooks();
+    void clearBookSearch();
+
+    // 读者管理
+    void addReader();
+    void editReader();
+    void deleteReader();
+    void searchReaders();
+    void clearReaderSearch();
+
+    // 借还书管理
+    void borrowBook();
+    void returnBook();
+    void renewBook();
+
+    // 统计
+    void refreshStatistics();
+    void generateReport();
+
+    // 逾期提醒
+    void checkOverdueBooks();
+    void showOverdueList();
+
+    // 系统
+    void setupDatabase();
+    void backupDatabase();
+    void restoreDatabase();
+    void about();
+
+    void createBookManagementTab();
+    void createReaderManagementTab();
+    void createBorrowReturnTab();
+    void createStatisticsTab();
+
+    QIcon createIcon(const QString &color, const QString &symbol);
+
+private:
+    void setupUI();
+    void createMenuBar();
+    void createToolBar();
+    void createStatusBar();
+    void createModels();
+    void applyFilters();
+
+    // UI组件
+    QTabWidget *tabWidget;
+
+    // 图书管理页
+    QTableView *bookTableView;
+    QSqlTableModel *bookModel;
+    QLineEdit *bookIdFilter;
+    QLineEdit *bookTitleFilter;
+    QLineEdit *bookAuthorFilter;
+    QLineEdit *bookIsbnFilter;
+    QComboBox *bookCategoryFilter;
+    QComboBox *bookStatusFilter;
+
+    // 读者管理页
+    QTableView *readerTableView;
+    QSqlTableModel *readerModel;
+    QLineEdit *readerIdFilter;
+    QLineEdit *readerNameFilter;
+    QLineEdit *readerPhoneFilter;
+    QComboBox *readerTypeFilter;
+
+    // 借阅记录页
+    QTableView *borrowTableView;
+    QSqlTableModel *borrowModel;
+
+    // 借还书操作
+    QLineEdit *borrowBookId;
+    QLineEdit *borrowReaderId;
+    QSpinBox *borrowDays;
+    QLineEdit *returnRecordId;
+
+    // 统计页
+    QLabel *totalBooksLabel;
+    QLabel *totalReadersLabel;
+    QLabel *borrowedBooksLabel;
+    QLabel *overdueBooksLabel;
+    QLabel *popularCategoryLabel;
+    QLabel *activeReadersLabel;
+    QTextEdit *reportTextEdit;
+
+    // 数据库
+    QSqlDatabase db;
+
+    // 定时器用于逾期检查
+    QTimer *overdueTimer;
+    QSystemTrayIcon *trayIcon;
+
+    // 模型
+    QStandardItemModel *statisticsModel;
+};
+
+#endif // LIBRARYMANAGER_H
---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿// main.cpp
+#include "librarymanager.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    // 设置应用程序信息
+    app.setApplicationName("Library Management System");
+    app.setOrganizationName("LibrarySoft");
+    app.setApplicationVersion("1.0.0");
+
+    LibraryManager window;
+    window.setWindowTitle("图书馆管理系统");
+    window.show();
+
+    return app.exec();
+}
